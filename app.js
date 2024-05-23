d78acd3cd1eb89fbdcad5b8d93429de9d141abf5
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2024-05-24 03:26:06 +0600

    testing resp

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ hambToggle.addEventListener('click', function(){
     hambToggle.classList.toggle('actn')
     small_d_menu.classList.toggle('active')
 })
+
+const textDtlsElements = document.querySelectorAll('.textDtls');
+
+textDtlsElements.forEach(textDtl => {
+  textDtl.addEventListener('click', function() {
+    
+    textDtlsElements.forEach(element => element.classList.remove('active'));
+    
+    this.classList.add('active');
+  });
+});
