e4bf59683a91e6f4826c24b266203a8a15fbe724
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2024-05-23 23:18:28 +0600

    all_res_done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let hambToggle = document.querySelector('.hamb_toggle');
-let small_d_menu = document.querySelector('.small_d_menu');
+let small_d_menu = document.querySelector('.toggleMenu');
 hambToggle.addEventListener('click', function(){
     hambToggle.classList.toggle('actn')
-    small_d_menu.classList.toggle('actvNav')
+    small_d_menu.classList.toggle('active')
 })
