23819739d6766f02f55e3cefcf15d5ac4194ba9d
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2024-05-24 21:26:32 +0600

    all test done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ hambToggle.addEventListener('click', function(){
 })
 
 const textDtlsElements = document.querySelectorAll('.textDtls');
+const imageElement = document.querySelector('.image img');
 
 textDtlsElements.forEach(textDtl => {
   textDtl.addEventListener('click', function() {
@@ -13,5 +14,56 @@ textDtlsElements.forEach(textDtl => {
     textDtlsElements.forEach(element => element.classList.remove('active'));
     
     this.classList.add('active');
+
+    if (this.id === 'step1') {
+        imageElement.src = './images/Rectangle 17.png';
+      } else if (this.id === 'step2') {
+        imageElement.src = './images/inlandBarging.jpg';
+      } else if (this.id === 'step3') {
+        imageElement.src = './images/t-2.jpg';
+      } else if (this.id === 'step4') {
+        imageElement.src = './images/t2.jpg';
+      }
+
+      let cost = document.querySelector('.cost');
+      let efficient = document.querySelector('.efficient');
+      let friendly = document.querySelector('.friendly');
+
+      if(this.id == 'cost'){
+        cost.classList.add('modiColor');
+        efficient.classList.remove('modiColor')
+        friendly.classList.remove('modiColor')
+      } else if( this.id == 'efficient'){
+        cost.classList.remove('modiColor');
+        efficient.classList.add('modiColor')
+        friendly.classList.remove('modiColor')
+      }else if( this.id == 'friendly'){
+        cost.classList.remove('modiColor');
+        efficient.classList.remove('modiColor')
+        friendly.classList.add('modiColor')
+      }
+
+      let p1 = document.querySelector('.p1');
+      let p2 = document.querySelector('.p2');
+      let p3 = document.querySelector('.p3');
+
+      if(this.id == 'cost' && window.innerWidth < 900){
+        p1.classList.add('actOn');
+        p2.classList.remove('actOn')
+        p3.classList.remove('actOn')
+        
+      } else if( this.id == 'efficient' && window.innerWidth < 900){
+        p1.classList.remove('actOn');
+        p2.classList.add('actOn')
+        p3.classList.remove('actOn')
+      }else if( this.id == 'friendly' && window.innerWidth < 900){
+        p1.classList.remove('actOn');
+        p2.classList.remove('actOn')
+        p3.classList.add('actOn')
+      }else if(window.innerWidth > 900){
+        p1.classList.remove('actOn');
+        p2.classList.remove('actOn')
+        p3.classList.remove('actOn')
+      }
   });
 });
